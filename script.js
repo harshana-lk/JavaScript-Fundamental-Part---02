30f6a0a6506e9715abbd8f4f744ff59068cde988
Harshana Jayaweera <harshanajayaweerayt@gmail.com>
2023-03-07 11:56:48 +0530

    Functions

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 "use strict";
 
-const hasDriversLicense = false;
-const passTest = true;
+// const hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log("I like to drive a car 🚗");
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("I like to drive a car 🚗");
+
+function logger() {
+  console.log("My name is Harshana");
+}
+
+// Calling / Invoking / Running Function
+logger();
+
+// ==>Parameterized Function
+function fruitProccesor(apple, orange) {
+  console.log(apple, orange);
+  const juice = `Juice with ${apple} Aplles and ${orange} Oranges .`;
+  return juice;
+}
+
+const appleJuice = fruitProccesor(5, 3);
+console.log(appleJuice);
+console.log(fruitProccesor(5, 8));
