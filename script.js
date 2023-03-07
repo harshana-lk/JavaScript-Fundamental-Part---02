84afb9dcda55cde0c46f48327ccba14e7c29afcf
Harshana Jayaweera <harshanajayaweerayt@gmail.com>
2023-03-07 12:18:26 +0530

    Function Declaration VS. Expressions

--- script.js
+++ script.js
@@ -6,20 +6,38 @@
 // if (passTest) hasDriverLicense = true;
 // if (hasDriversLicense) console.log("I like to drive a car 🚗");
 
-function logger() {
-  console.log("My name is Harshana");
+// function logger() {
+//   console.log("My name is Harshana");
+// }
+
+// // Calling / Invoking / Running Function
+// logger();
+
+// // ==>Parameterized Function
+// function fruitProccesor(apple, orange) {
+//   console.log(apple, orange);
+//   const juice = `Juice with ${apple} Aplles and ${orange} Oranges .`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProccesor(5, 3);
+// console.log(appleJuice);
+// console.log(fruitProccesor(5, 8));
+
+// ==>Function Declaration-------------------------------------------------------------------------------------------------------------------------------
+
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
 }
 
-// Calling / Invoking / Running Function
-logger();
+const calcAge = calcAge1(2000);
+console.log(calcAge);
 
-// ==>Parameterized Function
-function fruitProccesor(apple, orange) {
-  console.log(apple, orange);
-  const juice = `Juice with ${apple} Aplles and ${orange} Oranges .`;
-  return juice;
-}
+// ==>Annonymous / Expression Function-------------------------------------------------------------------------------------------------------------------
 
-const appleJuice = fruitProccesor(5, 3);
-console.log(appleJuice);
-console.log(fruitProccesor(5, 8));
+const myAge = function (birthYear) {
+  return 2023 - birthYear;
+};
+
+const calcAge2 = myAge(2000);
+console.log(calcAge2);
