033214ff52b0b02392f4996c68af02c3f91425d2
harshana-lk <harshanajayaweerayt@gmail.com>
2023-03-09 13:44:55 +0530

    Working with Arrays

--- script.js
+++ script.js
@@ -68,17 +68,53 @@
 
 // console.log(fruitProccesor(5, 2));
 
+// const calcAge = function (birthYear) {
+//   return 1950 - birthYear;
+// };
+
+// const yearsUntillRetirement = (birthYear, firstName) => {
+//   const retirement = calcAge(birthYear);
+//   if (retirement > 0) {
+//     return `${firstName} you are retiring in ${retirement} years`;
+//   } else {
+//     return `${firstName} you are already retired`;
+//   }
+// };
+
+// console.log(yearsUntillRetirement(2000, "Harshana"));
+
+const friend1 = "Nermo";
+const friend2 = "Garfield"; //==> Without Arrays
+const friend3 = "Olivia";
+
+const friends = ["Nermo", "Garfield", "Olivia"]; //==> With Arrays(Method 1)
+console.log(friends);
+const newFriends = new Array("Harshana", "Hansi"); //==> With Arrays(Method 2)
+console.log(newFriends);
+
+console.log(friends[0]); // To print an specific index in an Array
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Harshana";
+console.log(friends); // can change specified index value in an Array
+
+const firstName = "Harshana";
+const harshana = [firstName, "Jayaweera", 2023 - 2000, "Student", friends];
+console.log(harshana);
+console.log(harshana.length);
+
 const calcAge = function (birthYear) {
-  return 1950 - birthYear;
+  return 2023 - birthYear;
 };
 
-const yearsUntillRetirement = (birthYear, firstName) => {
-  const retirement = calcAge(birthYear);
-  if (retirement > 0) {
-    return `${firstName} you are retiring in ${retirement} years`;
-  } else {
-    return `${firstName} you are already retired`;
-  }
-};
+const year = [2000, 2001, 2002];
+console.log(calcAge(year)); // this cant be happen , this is wrong
 
-console.log(yearsUntillRetirement(2000, "Harshana"));
+console.log(calcAge(year[0]));
+console.log(calcAge(year[1]));
+console.log(calcAge(year[year.length - 1]));
+
+const age = [
+  calcAge(year[0], calcAge(year[1], calcAge(year[year.length - 1]))), // Arrays can assign methods (Expression - When they produces a value)
+];
+console.log(age);
